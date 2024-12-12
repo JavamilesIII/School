66f5f34d1f519254ed6939b678da95c0101acf04
Loïc Matthey <loicminecraft2.0@outlook.fr>
2024-12-12 21:47:07 +0100

    Add files via upload

---
+++ 2nd year/Java/Mehrere Fachklassen/Erweiterung/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"Erweiterung","l":"Besitzer"},{"p":"Erweiterung","l":"Schiff"},{"p":"Erweiterung","l":"Start"},{"p":"Erweiterung","l":"Wettkampf"}];updateSearchResults();
